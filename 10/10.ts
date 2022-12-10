042a99edd2f35ecbb82fc89fe0e46754ca0e734b
Nick Smedira <nsmedira@spotify.com>
2022-12-10 08:04:09 -0500

    Day 10 part 2

--- 10/10.ts
+++ 10/10.ts
@@ -43,3 +43,58 @@ const part1_redo = (() => {
 
     return signalStrengths.reduce((sum, strength) => sum + strength, 0);
 })();
+
+const part2 = (() => {
+    const instructions = [...input];
+
+    let x = 1;
+    let cycle = 1;
+
+    let addV: null | number = null;
+
+    let output = '';
+
+    const draw = () => {
+        const position = cycle % 40 > 0 ? (cycle % 40) - 1 : 39;
+
+        if (position >= x - 1 && position <= x + 1) {
+            output += 'X';
+            return;
+        }
+
+        output += '.';
+    };
+
+    while (cycle <= 240) {
+        draw();
+
+        if (cycle % 40 === 0) {
+            output += '\n';
+        }
+
+        if (typeof addV === 'number') {
+            x += addV;
+            addV = null;
+            cycle++;
+            continue;
+        }
+
+        const instruction = instructions.shift();
+
+        if (!instruction) {
+            break;
+        }
+
+        const [command, v] = instruction.split(' ');
+
+        if (command === 'noop') {
+            cycle++;
+            continue;
+        }
+
+        addV = Number(v);
+        cycle++;
+    }
+
+    return output;
+})();
